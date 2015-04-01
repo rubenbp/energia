27583420889a5a4aa1de83c6b5158a8434864015
Ruben Bernardez <rubenbp@gmail.com>
2015-04-01 02:18:41 +0200

    mejorando logs

--- server.js
+++ server.js
@@ -117,7 +117,10 @@ var SampleApp = function() {
     });
 
     self.app.get('/populate/year/:year', function(req, res) {
+      console.log("Petición para obtener datos del año " + req.params.year);
+
       var currentDay = moment(req.params.year).startOf('year');
+      console.log("Primer día: " + currentDay.toDate());
 
       res.set('Content-Type', 'text/html');
 
